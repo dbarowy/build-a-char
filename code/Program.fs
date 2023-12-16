7d2a01d4c7ef00f88d5f10b84b9ddf39f0fd5b51
Jess Hu <jesshu@Jesss-Air-2.lan>
2023-12-16 17:10:54 -0500

    made some more edits

--- code/Program.fs
+++ code/Program.fs
@@ -5,7 +5,7 @@ open System.IO
 let rec repl() : unit =
     let input = System.Console.ReadLine()
     if input = "quit" then
-        printfn "Goodbye!"
+        printfn "Goodbye! Thanks for using Build-A-Char! :)"
         exit 0
     else
         let asto = parse input
@@ -27,31 +27,31 @@ let rec repl() : unit =
 
 [<EntryPoint>]
 let main args =
-    printf "-----------------------------------------------------------------------------------------------------------------\n" 
-    printf "-----------------------------------------------------------------------------------------------------------------\n" 
+    printf "-----------------------------------------------------------------------------------------------------------------------\n" 
+    printf "-----------------------------------------------------------------------------------------------------------------------\n" 
     printf " \n Hello! Welcome to Jess Hu and Will Olsen's Build-A-Char! :D \n
-    -----------------------------------------------------------------------------------------------\n
+    -----------------------------------------------------------------------------------------------------------------------\n
     Build-A-Char will accept prompts that look like this: \n
     <emotion> <animal> <top> <bottom> <shoes> <accessory> \n
     and it will return a PNG called Character.PNG of a custom character fitting that description!\n
-    -----------------------------------------------------------------------------------------------\n
+    -----------------------------------------------------------------------------------------------------------------------\n
     Here are the available options for each category:\n
     <emotion>: happy, neutral, sad, mad, or tired\n
     <animal>: <color> bunny, cat, dog, or bear\n
-    <top>: <color> tshirt, sweater, hoodie, suit, dress or blank\n
-    <bottom>: <color> pants, shorts, skirt, or blank\n
-    <shoes>: <color> sneakers, cowboy boots, combat boots, heels, slippers, or blank \n
-    <accessory>: <color> flower, star, glasses, sunglasses, scarf, gloves, or blank\n
+    <top>: <color> tshirt, sweater, hoodie, suit, dress, or blank (if you do not want a top)\n
+    <bottom>: <color> pants, shorts, skirt, or blank (if you do not want a bottom)\n
+    <shoes>: <color> sneakers, cowboy boots, combat boots, heels, slippers, or blank (if you do not want shoes)\n
+    <accessory>: <color> flower, star, glasses, sunglasses, scarf, gloves, or blank (if you do not want an accessory)\n
     <color>: red, orange, yellow, green, blue, purple, pink, white, black, or brown\n
-    -----------------------------------------------------------------------------------------------\n
+    -----------------------------------------------------------------------------------------------------------------------\n
     Here are some example prompts:\n
     happy orange cat blue tshirt green pants black slippers pink flower\n 
     
-    sad brown bear pink dress purple cowboy boots blue scarf\n
+    sad brown bear pink dress white blank purple cowboy boots blue scarf\n
 
-    neutral white bunny black suit black sneakers yellow star\n \n"
-    printf "-----------------------------------------------------------------------------------------------------------------\n"
-    printf "-----------------------------------------------------------------------------------------------------------------\n" 
+    neutral white bunny black suit black blank black sneakers yellow star\n \n"
+    printf "-----------------------------------------------------------------------------------------------------------------------\n"
+    printf "-----------------------------------------------------------------------------------------------------------------------\n" 
     printf "Please enter a prompt! Type 'quit' when you are done using Build-A-Char!\n"
     repl()
     0
